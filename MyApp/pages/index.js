1a66d8264ce25880cd084875525fc227152cabac
Lewis Sneddon <>
2021-02-17 06:43:01 +0000

    class component in pages dir with render returning jsx

---
+++ MyApp/pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString()
+    }
+  }
+
+  tick() {
+    this.setState( () => {
+      return ({
+        time: new Date().toLocaleString()
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.interval = setInterval( () => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <h1>Hello: {this.state.time}</h1>
+  }
+}
+
+export default index;
